e2f459c556d8e3a21735e972ad252cd7b8df7c86
bhavik <kotharibhavik23@gmail.com>
2023-05-27 10:15:51 +0530

    done with some changes

--- lib/app/screens/set_up_profile/set_up_profile.dart
+++ lib/app/screens/set_up_profile/set_up_profile.dart
@@ -14,179 +14,181 @@ class SetUpProfile extends GetView<SetUpProfileController> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xfff7f6fb),
-      body: SingleChildScrollView(
-          child: Form(
-            key: formKey,
-            child: Padding(
-              padding: EdgeInsets.symmetric(vertical: 10.h, horizontal: 15.w),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  SizedBox(
-                    height: 20.h,
-                  ),
-                  Container(
-                    width: 200.w,
-                    height: 200.h,
-                    decoration: BoxDecoration(
-                      color: Colors.deepPurple.shade50,
-                      shape: BoxShape.circle,
+      body: SafeArea(
+        child: SingleChildScrollView(
+            child: Form(
+              key: formKey,
+              child: Padding(
+                padding: EdgeInsets.symmetric(vertical: 10.h, horizontal: 15.w),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    SizedBox(
+                      height: 20.h,
                     ),
-                    child: Image.asset(
-                      'assets/images/illustration-3.png',
+                    Container(
+                      width: 200.w,
+                      height: 200.h,
+                      decoration: BoxDecoration(
+                        color: Colors.deepPurple.shade50,
+                        shape: BoxShape.circle,
+                      ),
+                      child: Image.asset(
+                        'assets/images/illustration-3.png',
+                      ),
                     ),
-                  ),
-                  SizedBox(
-                    height: 20.h,
-                  ),
-                  Text(
-                    'Hey🖐!\nLooks like you are new here. Tell us a bit about yourself',
-                    style: TextStyle(
-                      fontSize: 20.sp,
-                      fontWeight: FontWeight.w400,
+                    SizedBox(
+                      height: 20.h,
                     ),
-                  ),
-                  SizedBox(
-                    height: 20.h,
-                  ),
-                  Container(
-                    padding: EdgeInsets.symmetric(vertical: 18.w, horizontal: 15.w),
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(12.r),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.black.withOpacity(0.2),
-                          blurRadius: 8.r,
-                          offset: const Offset(4, 4),
-                        ),
-                      ],
+                    Text(
+                      'Hey🖐!\nLooks like you are new here. Tell us a bit about yourself',
+                      style: TextStyle(
+                        fontSize: 20.sp,
+                        fontWeight: FontWeight.w400,
+                      ),
                     ),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: [
-                        TextFormField(
-                          style: const TextStyle(
-                            fontWeight: FontWeight.w500,
-                            color: Colors.black,
-                            letterSpacing: 2,
+                    SizedBox(
+                      height: 20.h,
+                    ),
+                    Container(
+                      padding: EdgeInsets.symmetric(vertical: 18.w, horizontal: 15.w),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(12.r),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withOpacity(0.2),
+                            blurRadius: 8.r,
+                            offset: const Offset(4, 4),
                           ),
-                          keyboardType: TextInputType.number,
-                          controller: controller.nameController,
-                          decoration: InputDecoration(
-                            enabledBorder: OutlineInputBorder(
-                              borderSide: const BorderSide(color: Colors.purple),
-                              borderRadius: BorderRadius.circular(10.r),
-
-                            ),
-                            focusedBorder: OutlineInputBorder(
-                              borderSide: const BorderSide(color: Colors.purple),
-                              borderRadius: BorderRadius.circular(10.r),
-
-                            ),
-                            labelText: "User Name",
-                            labelStyle: const TextStyle(
-                              color: Colors.black,
+                        ],
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          TextFormField(
+                            style: const TextStyle(
                               fontWeight: FontWeight.w500,
-                              letterSpacing: 0,
-                            ),
-                            filled: true,
-                            fillColor: Colors.white24,
-                          ),
-                        ),
-                        SizedBox(
-                          height: 20.h,
-                        ),
-                        TextFormField(
-                          style: const TextStyle(
-                            fontWeight: FontWeight.w500,
-                            color: Colors.white,
-                            letterSpacing: 2,
-                          ),
-                          keyboardType: TextInputType.emailAddress,
-                          controller: controller.emailController,
-                          decoration: InputDecoration(
-                            enabledBorder: OutlineInputBorder(
-                              borderSide: const BorderSide(color: Colors.purple),
-                              borderRadius: BorderRadius.circular(10.r),
-                            ),
-                            focusedBorder: OutlineInputBorder(
-                              borderSide: const BorderSide(color: Colors.purple),
-                              borderRadius: BorderRadius.circular(10.r),
-
-                            ),
-                            labelText: "Email",
-                            labelStyle: const TextStyle(
                               color: Colors.black,
-                              fontWeight: FontWeight.w500,
-                              letterSpacing: 0,
+                              letterSpacing: 2,
                             ),
-                            filled: true,
-                            fillColor: Colors.white24,
+                            keyboardType: TextInputType.number,
+                            controller: controller.nameController,
+                            decoration: InputDecoration(
+                              enabledBorder: OutlineInputBorder(
+                                borderSide: const BorderSide(color: Colors.purple),
+                                borderRadius: BorderRadius.circular(10.r),
+
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderSide: const BorderSide(color: Colors.purple),
+                                borderRadius: BorderRadius.circular(10.r),
+
+                              ),
+                              labelText: "User Name",
+                              labelStyle: const TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.w500,
+                                letterSpacing: 0,
+                              ),
+                              filled: true,
+                              fillColor: Colors.white24,
+                            ),
                           ),
-                        ),
-                        SizedBox(
-                          height: 10.h,
-                        ),
-                        Padding(
-                          padding: EdgeInsets.symmetric(vertical: 10.w),
-                          child: ElevatedButton(
-                            onPressed: () async {
-                              if (formKey.currentState!.validate()) {
-                                if(GetUtils.isEmail(controller.emailController.text)){
-                                  await controller.setUpUserProfile();
-                                }else{
-                                  Get.snackbar(
-                                    '',
-                                    '',
-                                    titleText: Text(
-                                      'Email Verification',
-                                      style: TextStyle(
-                                          fontWeight: FontWeight.w700,
-                                          fontSize: 16.sp
+                          SizedBox(
+                            height: 20.h,
+                          ),
+                          TextFormField(
+                            style: const TextStyle(
+                              fontWeight: FontWeight.w500,
+                              color: Colors.white,
+                              letterSpacing: 2,
+                            ),
+                            keyboardType: TextInputType.emailAddress,
+                            controller: controller.emailController,
+                            decoration: InputDecoration(
+                              enabledBorder: OutlineInputBorder(
+                                borderSide: const BorderSide(color: Colors.purple),
+                                borderRadius: BorderRadius.circular(10.r),
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderSide: const BorderSide(color: Colors.purple),
+                                borderRadius: BorderRadius.circular(10.r),
+
+                              ),
+                              labelText: "Email",
+                              labelStyle: const TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.w500,
+                                letterSpacing: 0,
+                              ),
+                              filled: true,
+                              fillColor: Colors.white24,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 10.h,
+                          ),
+                          Padding(
+                            padding: EdgeInsets.symmetric(vertical: 10.w),
+                            child: ElevatedButton(
+                              onPressed: () async {
+                                if (formKey.currentState!.validate()) {
+                                  if(GetUtils.isEmail(controller.emailController.text)){
+                                    await controller.setUpUserProfile();
+                                  }else{
+                                    Get.snackbar(
+                                      '',
+                                      '',
+                                      titleText: Text(
+                                        'Email Verification',
+                                        style: TextStyle(
+                                            fontWeight: FontWeight.w700,
+                                            fontSize: 16.sp
+                                        ),
                                       ),
-                                    ),
-                                    messageText: Text(
-                                      'Please enter a valid email Id',
-                                      style: TextStyle(
-                                          fontWeight: FontWeight.w500,
-                                          fontSize: 12.sp
+                                      messageText: Text(
+                                        'Please enter a valid email Id',
+                                        style: TextStyle(
+                                            fontWeight: FontWeight.w500,
+                                            fontSize: 12.sp
+                                        ),
                                       ),
-                                    ),
-                                    margin: EdgeInsets.symmetric(
-                                      horizontal: 10.w,
-                                      vertical: 10.w,
-                                    ),
-                                    backgroundColor: Colors.grey[300],
-                                    snackPosition: SnackPosition.BOTTOM,
-                                  );
+                                      margin: EdgeInsets.symmetric(
+                                        horizontal: 10.w,
+                                        vertical: 10.w,
+                                      ),
+                                      backgroundColor: Colors.grey[300],
+                                      snackPosition: SnackPosition.BOTTOM,
+                                    );
+                                  }
                                 }
-                              }
-                            },
-                            style: ElevatedButton.styleFrom(
-                              backgroundColor: Colors.purple,
-                            ),
-                            child: Container(
-                              padding: EdgeInsets.symmetric(vertical: 15.w),
-                              child: Text(
-                                "Submit",
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.w500,
-                                  fontSize: 18.sp,
+                              },
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: Colors.purple,
+                              ),
+                              child: Container(
+                                padding: EdgeInsets.symmetric(vertical: 15.w),
+                                child: Text(
+                                  "Submit",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.w500,
+                                    fontSize: 18.sp,
+                                  ),
                                 ),
                               ),
                             ),
                           ),
-                        ),
-                      ],
-                    ),
-                  )
-                ],
+                        ],
+                      ),
+                    )
+                  ],
+                ),
               ),
             ),
           ),
-        ),
+      ),
     );
   }
 }
